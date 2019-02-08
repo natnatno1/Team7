9949dda79135df50b4e6893ff164e6c52af789bf
natnatno1 <44027070+natnatno1@users.noreply.github.com>
2019-02-08 13:01:59 +0000

    Natalie
    -More Models Added
    -Score Behaviour started

---
+++ Team7/Assets/Natalie/NC_Scripts/Target_Behaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target_Behaviour : MonoBehaviour
+{
+
+    public int TargetScore;
+    public float Health;
+    public GameManager_Script GM_Script;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GM_Script = GameObject.Find("Game_Manager").GetComponent<GameManager_Script>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+        {
+            GM_Script.SendMessage("AddScore", TargetScore);
+            //Destroy(this.gameObject);
+        }
+    }
+}
